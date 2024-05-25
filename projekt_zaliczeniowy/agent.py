1abebb5269c34c2c8538d81ff78ce4e935d9518d
Szymon Skalski 2 <ul0246831@edu.uni.lodz.pl>
2024-05-25 09:39:05 +0200

    fix the behaviour of agent.py when being imported

--- projekt_zaliczeniowy/agent.py
+++ projekt_zaliczeniowy/agent.py
@@ -40,42 +40,43 @@ class DeepQAgent:
 
     def decay_exploration_rate(self):
         self.exploration_rate = max(self.min_exploration_rate, self.exploration_rate * self.exploration_decay_rate)
-env = game.SnakeGameEnv()
-env.reset()
 
-
-num_states = 7
-num_actions = 4
-
-agent = DeepQAgent(num_actions,num_states)
-
-num_episodes = 100000
-for episode in range(num_episodes):
-    state = np.array(list(env.reset().values())).ravel().reshape(1, -1)
-    #print(state)
-    done = False
-    total_reward = 0
-
-    while not done:
-
-        action = agent.choose_action(state)
-        #print(state)
-        
-        next_state, reward, done, info = env.step(action)
-        next_state = np.array(list(next_state.values())).ravel().reshape(1, -1)
-        
-        agent.update_q_values(state, action, reward, next_state,done)
-        state=next_state
-
-        #env.render()
-        if done:
-            break
-        
-        total_reward += reward
-        
-    print(f"Episode {episode + 1}: Total Reward = {total_reward}")
-    agent.decay_exploration_rate()
-
-agent.model.save('trained_model.h5')
 
 if __name__ == "__main__":
+    env = game.SnakeGameEnv()
+    env.reset()
+
+
+    num_states = 7
+    num_actions = 4
+
+    agent = DeepQAgent(num_actions,num_states)
+
+    num_episodes = 100000
+    for episode in range(num_episodes):
+        state = np.array(list(env.reset().values())).ravel().reshape(1, -1)
+        #print(state)
+        done = False
+        total_reward = 0
+
+        while not done:
+
+            action = agent.choose_action(state)
+            #print(state)
+            
+            next_state, reward, done, info = env.step(action)
+            next_state = np.array(list(next_state.values())).ravel().reshape(1, -1)
+            
+            agent.update_q_values(state, action, reward, next_state,done)
+            state=next_state
+
+            #env.render()
+            if done:
+                break
+            
+            total_reward += reward
+            
+        print(f"Episode {episode + 1}: Total Reward = {total_reward}")
+        agent.decay_exploration_rate()
+
+    agent.model.save('trained_model.h5')
